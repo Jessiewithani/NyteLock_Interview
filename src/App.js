4ccee6a7813b1cd32350e8e57406dca7a7f51e06
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-06-28 16:31:18 -0600

    Create sections inside logo container

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ function App() {
   return (
     <div className="App">
       <h2>APP</h2>
+      {/* this is where the header section with the button would be */}
+      <section className="logo-services-container">
+        <div className="logo-services-wrap">
+          <section className="logo-header">
+            
+          </section>
+          <section className="logo-types">
+
+          </section>
+        </div>
+      </section>
+      {/* this is where the types of logo styles would be */}
     </div>
   );
 }

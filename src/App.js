16f2439997bd805dcb79094eeb0c81271184fa14
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-06-28 17:01:51 -0600

    Finish basic HTML structure for first logo section

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ function App() {
       {/* this is where the header section with the button would be */}
       <section className="logo-services-container">
         <div className="logo-services-wrap">
-          <section className="logo-header">
-            
+          <section className="logo-header-container">
+            <div className="heading-container">
+              <h6 className="sub-logo-heading"><span>How do I know which logo is right for me?</span></h6>
+              <h2 className="main-logo-heading">Differnt Types of Logo Styles</h2>
+            </div>
+            <div className="contact-btn-container">
+              <button>Contact Us</button>
+            </div>
           </section>
-          <section className="logo-types">
-
+          <section className="logo-types-container">
+            {/* In here would be the different logo cards */}
           </section>
         </div>
       </section>

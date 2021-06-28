dbcfc73ab2ddcb894d4d5a7717cbabb623fd6ee3
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-06-28 17:39:52 -0600

    Create data for logo types

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import LogoTypesContainer from './components/LogoTypesContainer';
 function App() {
   return (
     <div className="App">
-      <h2>APP</h2>
       {/* this is where the header section with the button would be */}
       <section className="logo-services-container">
         <div className="logo-services-wrap">
@@ -14,9 +13,16 @@ function App() {
           <LogoTypesContainer />
         </div>
       </section>
+      <section className="logo-description-container">
+        {/* this is where the description component would be */}
+      </section>
       {/* this is where the types of logo styles would be */}
     </div>
   );
 }
 
 export default App;
+
+// 1. Build the basic HTML layout and add basic CSS for the page based off of a wireframe I created
+// 2. Convert the HTML layout to components
+// 3. Now that I have my components, I need to create a file with my needed data
---
+++ src/LogoTypes.js
@@ -0,0 +1,38 @@
+const LogoTypes = [
+  {
+    icon: '',
+    title: 'Lettermark Logos',
+    url: '#',
+    id: 1
+  },
+  {
+    icon: '',
+    title: 'Wordmark Logos',
+    url: '#',
+    id: 2
+  },
+  {
+    icon: '',
+    title: 'Pictorial Mark Logos',
+    url: '#',
+    id: 3
+  },
+  {
+    icon: '',
+    title: 'Abstract Mark Logos',
+    url: '#',
+    id: 4
+  },
+  {
+    icon: '',
+    title: 'Mascot Logos',
+    url: '#',
+    id: 5
+  },
+  {
+    icon: '',
+    title: 'Combination Logos',
+    url: '#',
+    id: 6
+  }
+]

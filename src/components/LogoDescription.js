1317206cfbf129e678cac6e82a79d24325c0f83d
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-06-29 23:05:28 -0600

    Set up description container in LogoDescription
    along with some basic styling in the LogoDescription component

--- src/components/LogoDescription.js
+++ src/components/LogoDescription.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import '../LogoDescription.css'
 
 const LogoDescription = () => {
   return (
     <section className="logo-description-wrapper">
-      <div>
-        <h2>Description Logo</h2>
+      <div className="logo-description-section">
+        <div className="logo-title-container">
+          <h2 className="logo-title">Description Logo</h2>
+        </div>
+        <div className="logo-detail-container">
+          <p className="logo-detail">this is the description about the logo that you chose</p>
+        </div>
       </div>
-      <div>
-        <p>this is the description about the logo that you chose</p>
+      <div className="logo-examples">
+
       </div>
     </section>
   )

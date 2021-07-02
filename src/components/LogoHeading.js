264d17a45c03a375512ba4468e1b3ee2678acbb9
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-07-01 22:45:21 -0600

    Style and add font to sub heading and main heading

--- src/components/LogoHeading.js
+++ src/components/LogoHeading.js
@@ -6,10 +6,12 @@ const LogoHeading = () => {
     <section className="logo-heading-container">
       <div className="heading-wrapper">
         <div className="sub-heading">
-          <h6>How do I know which logo style is right for me?</h6>
+          <h6 className="h6-heading">
+            <span className="sub-heading-span">{"How do I know which logo style is right for me?".toUpperCase()}</span>
+          </h6>
         </div>
         <div className="main-heading">
-          <h2>Different Types of Logo Styles</h2>
+          <h2 className="h2-main-heading">Different Types of Logo Styles</h2>
         </div>
       </div>
       <div className="contact-btn-container">
